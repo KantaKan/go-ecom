0a6be6ddb0d1720ee37f447ffe6ccd9c896ccd87
fiadaze <fiakantapon@gmail.com>
2025-11-21 01:08:57 +0700

    feat(api): added some middleware from docs

--- cmd/api.go
+++ cmd/api.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-  "net/http"
 )
 
 type application struct {
@@ -12,17 +14,25 @@ type application struct {
 
 // mount
 func (app *application) mount() http.Handler {
-  r := chi.NewRouter()
-  r.Use(middleware.Logger)
-  r.Use(middleware.Recoverer)
+	r := chi.NewRouter()
 
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-  r.Get("/", func(w http.ResponseWriter, r *http.Request){
-    w.Write([]byte("root."))
-  })
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
 
-  http.ListenAndServe(":3333", r)
-  return r 
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	//http.ListenAndServe(":3333", r)
+	return r
 }
 
 type config struct {
