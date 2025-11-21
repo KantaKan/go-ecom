097237bf02d0eeec3342c2380b4bc36fead5a964
KantaKan <fiakantapon@gmail.com>
2025-11-21 14:23:16 +0700

    added run() function

--- cmd/api.go
+++ cmd/api.go
@@ -8,10 +8,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type application struct {
-	config config
-}
-
 // mount
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
@@ -20,21 +16,37 @@ func (app *application) mount() http.Handler {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
+	r.Use(middleware.Recoverer) // recover from panics and write a 500 status code
 
 	// Set a timeout value on the request context (ctx), that will signal
 	// through ctx.Done() that the request has timed out and further
 	// processing should be stopped.
-	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.Timeout(60 * time.Second)) //idk man this is context something if i remember correctly
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good"))
 	})
 
-	//http.ListenAndServe(":3333", r)
 	return r
 }
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout: time.Minute
+	}
+	
 
+
+	log.Printf("Server is running on %s", app.config.addr)
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+}
 type config struct {
 	addr string
 	db   dbConfig
