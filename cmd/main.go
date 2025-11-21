ab2ea76c6f814e8a7fc84a9f31374df8b958a564
KantaKan <fiakantapon@gmail.com>
2025-11-21 14:29:07 +0700

    feat(main): api.run server config with error handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
 	cfg := config{
 		addr: ":8080",
@@ -7,6 +12,11 @@ func main() {
 	}
 
 	api := application{
-		config: cfg
+		config: cfg,
+	}
+
+	if err := api.run(api.mount()); err != nil {
+		log.Printf("Server failed to start ,err : %s", err)
+		os.Exit(1)
 	}
 }
